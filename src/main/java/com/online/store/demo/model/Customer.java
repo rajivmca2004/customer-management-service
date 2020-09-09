c8540c9bc1b63ab22bff9a029797f8b6cd946287
Rajiv Srivastava <rajivmca2004@gmail.com>
2020-09-09 19:45:31 +0530

    Removed Thyeleaf code

--- src/main/java/com/online/store/demo/model/Customer.java
+++ src/main/java/com/online/store/demo/model/Customer.java
@@ -10,7 +10,6 @@ import javax.persistence.Id;
  * @author rasrivastava
  *
  */
-@SuppressWarnings("unused")
 @Entity
 public class Customer {
 	
@@ -33,9 +32,32 @@ public class Customer {
         this.email = email;
     }
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+    
 	@Override
 	public String toString() {
 		return "Customer [id=" + id + ", name=" + name + ", email=" + email + "]";
 	}
-    
 }
